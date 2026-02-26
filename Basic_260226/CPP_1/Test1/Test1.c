e7c3bce8f86ac4180ec43fab00c3552c92bdd4a4
Kim-Ye-Sung <rhkwk6351@naver.com>
2026-02-26 17:32:40 +0900

    260226 수업내용정리

---
+++ Basic_260226/CPP_1/Test1/Test1.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int n;
+
+int main()
+{
+	char arr[] = "hong";
+	char *arr1 = "hong";
+	
+	//printf("arr : %c", arr);
+	printf("arr[0] : %c\n", arr[0]);
+	printf("arr[1] : %c\n", arr[1]);
+	printf("arr[2] : %c\n", arr[2]);
+	printf("arr[3] : %c\n", arr[3]);
+	printf("arr[4] : %s\n", arr[4]);
+
+	printf("arr : %d\n", arr);
+	printf("arr1 : %d\n", &arr1);
+	printf("hong : %d\n", *arr1);
+	printf("n : %d\n", &n);
+
+	return 0;
+}
---
+++ Basic_260226/CPP_1/Test2/Test2.c
@@ -0,0 +1,39 @@
+﻿// 전역, 지역, 스태틱, 포인터 변수들은 메모리의 어느 지역에 저장될까?
+
+#include <stdio.h>
+
+int a;
+int global_c = 100;
+
+int func()
+{
+	return 1;
+}
+
+int main()
+{
+	int b;
+	static int c;
+	const int d;
+	char arr[20] = "hi";
+	char* parr = "hong";
+
+	printf("func     : %p\n", func);	// 함수 이름도 주소
+	printf("global a : %p\n", &a);
+	printf("local b  : %p\n", &b);
+	printf("global_C : %p\n", &global_c);
+	printf("static c : %p\n", &c);
+	printf("const d  : %p\n", &d);
+	printf("array    : %p\n", arr);
+	printf("parr     : %p\n", parr);
+
+	return 0;
+}
+
+/*
+	메모리 구조
+	data segment : stack (함수가 실행될때마다 할당받는 영역, 지역변수등이 할당됨. 그래서 함수가 종료되면 할당된 변수들이 반환됨)
+				   heap 
+				   data
+	code segment : 함수 하나당 여기에 할당
+*/
